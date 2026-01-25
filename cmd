bisgardo/cmd

commit 39e8cd73687f916d86dadb23a7bb773ef93816f0

Add helper for including other commands by path relative to cmd's own location
A new conveinence variable `$cmd_dir` (defined as `dirname "$cmd_script"`) is exposed to both eval exprs and scripts. To easily include other cmds (might contain env setup or something), the convenience function `cmd_include` uses this var to include another script by path relative to its own (not taking `CMD_ROOTS` into consideration).

Added a test that it works, as well as calling `cmd_run` directly to resolve other commands from all roots also does.

Includes a bunch of cleanup like defaulting to wrapping strings in single quotes in tests (reduces the need for escaping in some cases). Also renamed and "exported" `cmd_command` (though it seems like scripts maybe shouldn't really need it as they can go through `cmd_run`).

cmd
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 CMD_ROOTS="${CMD_ROOTS-.}"
-_cmd_name="$(basename "$0")"
+cmd_command="$0"
 
 # UTILITIES #
 
@@ -47,6 +47,11 @@ function cmd_join {
   echo -e "$res"
 }
 
+function cmd_include {
+  local path="$1"
+  source "$cmd_dir/$path$CMD_SUFFIX"
+}
+
 # RESOLVER #
 
 CMD_SUFFIX=.cmd
@@ -56,6 +61,7 @@ function _cmd_echo_root_run_script {
   local root="$1"
   local path_from_root="$2"
   if shift 2; then
+    # TODO: Accept that cmd_path may leave root dir or prevent it. Add a test either way.
     local cmd_path="$root/$path_from_root$CMD_SUFFIX"
     if [ -e "$cmd_path" ]; then
       # Outputs script of the form `cmd_root=... cmd_script=root/p1/p2.cmd $func [args]`.
@@ -82,7 +88,7 @@ function _cmd_echo_unique_run_script {
   local r
   while read -r r; do run_scripts+=("$r"); done <<< "$(_cmd_echo_run_scripts "$@")"
 	if [ -z "$run_scripts" ]; then
-		cmd_log "$_cmd_name: command \"$1\" not found"
+		cmd_log "$cmd_command: command \"$1\" not found"
 		return 1
 	fi
 	if [ "${#run_scripts[@]}" -gt 1 ]; then
@@ -92,7 +98,7 @@ function _cmd_echo_unique_run_script {
 	  }
 	  local scripts_joined=$(for r in "${run_scripts[@]}"; do func=__cmd_echo_script eval "$r"; done | cmd_join ', ')
 	  unset __cmd_echo_script
-	  cmd_log "$_cmd_name: ambiguous command (matched: $scripts_joined)"
+	  cmd_log "$cmd_command: ambiguous command (matched: $scripts_joined)"
 	  return 2
   fi
   echo "$run_scripts" # single-element-array
@@ -103,12 +109,15 @@ function cmd_eval {
   function __cmd_eval {
     # args: cmd_args...
     # scope: cmd_root, cmd_script, ...
-    if [ "${cmd_script-}" ]; then cmd_log "# [$cmd_script]"; fi
+    if [ "${cmd_script-}" ]; then
+      cmd_log "# [$cmd_script]"
+      local cmd_dir="$(dirname "$cmd_script")" # expose to script for convenience
+    fi
     cmd_log "> $eval_expr"
     # Eval user-provided expression. Everything in scope is inherited, including args (available as "$@").
     local x=0; eval "$eval_expr" || x=$?
     if [ "$x" -ne 0 ]; then
-      cmd_log "$_cmd_name: eval expression failed with exit code $x"
+      cmd_log "$cmd_command: eval expression failed with exit code $x"
       return 4
     fi
   }
@@ -145,6 +154,7 @@ function cmd_run {
   function __cmd_source_script {
     # args: cmd_args...
     # scope: cmd_root, cmd_script, ...
+    local cmd_dir="$(dirname "$cmd_script")" # expose to script for convenience
     source "$cmd_script" "$@" # inherits everything in scope
   }
   local run_script # must declare local first as it otherwise eats the called function's return value

test.sh
@@ -1,12 +1,12 @@
 function cmd {
-  CMD_ROOTS=testdata/root1:testdata/root2 ./cmd "$@"
+  CMD_ROOTS='testdata/root1:testdata/root2:testdata/spaced root' ./cmd "$@"
 }
 
 function test_can_run {
   local out # must declare 'local' before assignment for exit code to propagate...
   out=$(cmd 2>&1)
   assertEquals 1 $?
-  assertContains "$out" "cmd is a tool"
+  assertContains "$out" 'cmd is a tool'
 }
 
 function test_can_run_hello {
@@ -14,7 +14,7 @@ function test_can_run_hello {
   local out
   out=$(cmd hello 2>&1)
   assertEquals 0 $?
-  assertEquals "Hello, world!" "$out"
+  assertEquals 'Hello, world!' "$out"
 }
 
 function test_can_run_nested_hello {
@@ -22,10 +22,10 @@ function test_can_run_nested_hello {
   local out
   out=$(cmd nested/hello 2>&1)
   assertEquals 0 $?
-  assertEquals "Hello, nested world!" "$out"
+  assertEquals 'Hello, nested world!' "$out"
   out=$(cmd nested hello 2>&1)
   assertEquals 1 $?
-  assertEquals "cmd: command \"nested\" not found" "$out"
+  assertEquals './cmd: command "nested" not found' "$out"
 }
 
 function test_can_run_echo {
@@ -33,21 +33,21 @@ function test_can_run_echo {
   local out
   out=$(cmd echo hello echo 2>&1)
   assertEquals 0 $?
-  assertEquals "hello echo" "$out"
+  assertEquals 'hello echo' "$out"
 }
 
 function test_cannot_run_nonexistent {
   local out
   out=$(cmd nonexistent 2>&1)
   assertEquals 1 $?
-  assertEquals "cmd: command \"nonexistent\" not found" "$out"
+  assertEquals './cmd: command "nonexistent" not found' "$out"
 }
 
 function test_cannot_run_ambiguous {
   local out
   out=$(CMD_ROOTS=testdata/root1:testdata/root1/nested ./cmd hello 2>&1)
   assertEquals 2 $?
-  assertEquals "cmd: ambiguous command (matched: testdata/root1/hello.cmd, testdata/root1/nested/hello.cmd)" "$out"
+  assertEquals './cmd: ambiguous command (matched: testdata/root1/hello.cmd, testdata/root1/nested/hello.cmd)' "$out"
 }
 
 function test_can_handle_quotes {
@@ -60,16 +60,19 @@ function test_can_handle_quotes {
 
 function test_eval_env {
   local out
-  out=$(cmd --eval 'echo cmd_root=$cmd_root cmd_script=$cmd_script' hello 2>/dev/null)
+  out=$(cmd --eval 'echo cmd_root=$cmd_root cmd_script=$cmd_script cmd_dir=$cmd_dir' hello 2>/dev/null)
   assertEquals 0 $?
-  assertEquals "cmd_root=testdata/root1 cmd_script=testdata/root1/hello.cmd" "$out"
+  assertEquals 'cmd_root=testdata/root1 cmd_script=testdata/root1/hello.cmd cmd_dir=testdata/root1' "$out"
+  out=$(cmd --eval 'echo cmd_root=$cmd_root cmd_script=$cmd_script cmd_dir=$cmd_dir' nested/hello 2>/dev/null)
+  assertEquals 0 $?
+  assertEquals 'cmd_root=testdata/root1 cmd_script=testdata/root1/nested/hello.cmd cmd_dir=testdata/root1/nested' "$out"
 }
 
 function test_eval_echo_args {
   local out
   out=$(cmd --eval 'echo $@' hello a b c 2>/dev/null)
   assertEquals 0 $?
-  assertEquals "a b c" "$out"
+  assertEquals 'a b c' "$out"
 }
 
 function test_eval_quoted {
@@ -87,7 +90,7 @@ function test_eval_quoted_unmatched {
   out=$(cmd --eval ec\'ho hello 2>&1)
   assertEquals 4 $?
   assertContains "$out" "unexpected EOF while looking for matching \`'"
-  assertContains "$out" "eval expression failed with exit code"
+  assertContains "$out" 'eval expression failed with exit code'
 }
 
 function test_eval_return {
@@ -104,14 +107,14 @@ function test_cannot_eval_ambiguous {
   local out
   out=$(CMD_ROOTS=testdata/root1:testdata/root1/nested ./cmd --eval 'echo goodbye' hello 2>&1)
   assertEquals 2 $?
-  assertEquals "cmd: ambiguous command (matched: testdata/root1/hello.cmd, testdata/root1/nested/hello.cmd)" "$out"
+  assertEquals './cmd: ambiguous command (matched: testdata/root1/hello.cmd, testdata/root1/nested/hello.cmd)' "$out"
 }
 
 function test_eval_empty {
   # Run "eval" outside the context of a command.
   # Use case: access internal utility without having to create a .cmd file.
   local out
-  out=$(cmd --eval 'cmd_split ,' <<< "a,b,c" 2>&1)
+  out=$(cmd --eval 'cmd_split ,' <<< 'a,b,c' 2>&1)
   assertEquals 0 $?
   assertEquals $'> cmd_split ,\na\nb\nc' "$out"
   out=$(cmd --eval 'echo "$@"' -- x y z 2>&1)
@@ -124,20 +127,27 @@ function test_stdin {
   local out
   out=$((echo the quick; echo lazy dog) | cmd wc 2>&1)
   assertEquals 0 $?
-  assertEquals "2 4 19" "$(echo $out)" # let unquoted echo collapse whitespace to eliminate platform differences of `wc`
+  assertEquals '2 4 19' "$(echo $out)" # let unquoted echo collapse whitespace to eliminate platform differences of `wc`
   out=$((echo the quick; echo lazy dog) | cmd --eval wc hello 2>/dev/null)
   assertEquals 0 $?
-  assertEquals "2 4 19" "$(echo $out)"
+  assertEquals '2 4 19' "$(echo $out)"
   out=$((echo the quick; echo lazy dog) | cmd --eval=wc hello 2>/dev/null)
   assertEquals 0 $?
-  assertEquals "2 4 19" "$(echo $out)"
+  assertEquals '2 4 19' "$(echo $out)"
+}
+
+function test_including {
+  local out
+  out=$(cmd including)
+  assertEquals 0 $?
+  assertEquals $'Hello from included!\nHello with love!\nHello from including!\nHello with prompts!\nHello, world!\nHello, world!' "$out"
 }
 
 function test_list {
   local out
   out=$(cmd --list 2>&1)
   assertEquals 0 $?
-  assertEquals $'# testdata/root1\nhello\nnested/hello\n# testdata/root2\nwc\necho' "$out"
+  assertEquals $'# testdata/root1\nhello\nnested/hello\n# testdata/root2\nwc\necho\n# testdata/spaced root\nincluding' "$out"
 }
 
 # ---

testdata/included.cmd
@@ -1,0 +1,7 @@
+hello_from included # function provided by root2/including.cmd
+
+function hello_with {
+    echo "Hello with $1!"
+}
+
+hello_with love

testdata/root1/hello.cmd
@@ -1,1 +1,5 @@
-echo 'Hello, world!'
+function hello_world {
+    echo 'Hello, world!'
+}
+
+hello_world

testdata/spaced root/including.cmd
@@ -1,0 +1,11 @@
+function hello_from {
+    echo "Hello from $1!"
+}
+
+cmd_include ../included  # include using relative path (omitting '.cmd') - doesn't take CMD_ROOTS into account
+
+hello_from including     # call function defined above
+hello_with prompts       # call function defined in 'included.cmd'
+
+cmd_run hello            # includes/runs 'root1/hello.cmd' (note that it belongs to another root)
+hello_world              # call function defined in 'hello.cmd' (was also called by script itself)

commit 6d065b0716f330d74b324826bf402952895f8671

Let `cmd_command` be basename of `$0`
This makes error messages look nicer. Was broken in 39e8cd73687f916d86dadb23a7bb773ef93816f0, just a few commits ago.

cmd
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 CMD_ROOTS="${CMD_ROOTS-.}"
-cmd_command="$0"
+cmd_command="$(basename $0)"
 
 # UTILITIES #
 
@@ -170,7 +170,7 @@ function cmd_run {
 
 if [ "$#" -eq 0 ]; then
   cmd_log "cmd is a tool for finding and running commands."
-  cmd_log "Usage: cmd <command> [args]"
+  cmd_log "Usage: $cmd_command <command> [args]"
   cmd_log "Runs <root>/<command>.cmd, where <root> is a member of the set configured in env var CMD_ROOTS as ':'-separated paths."
   exit 1
 fi

test.sh
@@ -25,7 +25,7 @@ function test_can_run_nested_hello {
   assertEquals 'Hello, nested world!' "$out"
   out=$(cmd nested hello 2>&1)
   assertEquals 1 $?
-  assertEquals './cmd: command "nested" not found' "$out"
+  assertEquals 'cmd: command "nested" not found' "$out"
 }
 
 function test_can_run_echo {
@@ -40,14 +40,14 @@ function test_cannot_run_nonexistent {
   local out
   out=$(cmd nonexistent 2>&1)
   assertEquals 1 $?
-  assertEquals './cmd: command "nonexistent" not found' "$out"
+  assertEquals 'cmd: command "nonexistent" not found' "$out"
 }
 
 function test_cannot_run_ambiguous {
   local out
   out=$(CMD_ROOTS=testdata/root1:testdata/root1/nested ./cmd hello 2>&1)
   assertEquals 2 $?
-  assertEquals './cmd: ambiguous command (matched: testdata/root1/hello.cmd, testdata/root1/nested/hello.cmd)' "$out"
+  assertEquals 'cmd: ambiguous command (matched: testdata/root1/hello.cmd, testdata/root1/nested/hello.cmd)' "$out"
 }
 
 function test_can_handle_quotes {
@@ -108,7 +108,7 @@ function test_cannot_eval_ambiguous {
   local out
   out=$(CMD_ROOTS=testdata/root1:testdata/root1/nested ./cmd --eval 'echo goodbye' hello 2>&1)
   assertEquals 2 $?
-  assertEquals './cmd: ambiguous command (matched: testdata/root1/hello.cmd, testdata/root1/nested/hello.cmd)' "$out"
+  assertEquals 'cmd: ambiguous command (matched: testdata/root1/hello.cmd, testdata/root1/nested/hello.cmd)' "$out"
 }
 
 function test_eval_empty {